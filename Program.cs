1778697757321df7499ac74ef30e768dfc6bfe74
joseamorim <jpoa95@gmail.com>
2025-08-18 12:15:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿myLoop:
+int number;
+Console.WriteLine("Choose your multiplication table.");
+bool input = int.TryParse(Console.ReadLine(), out number);
+
+if (input)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine(number + "x" + i + "=" + number * i);
+    }
+    goto myLoop;
+}
